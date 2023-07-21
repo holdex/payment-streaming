06705fb0e19bc0aedc66a21fff6e0411b3e9a060
CTAPCKPIM <novickdanya@gmail.com>
2023-07-21 19:53:53 +0300

    test: rewrited tests for 'deposit();'

--- test/StreamManagerTest.js
+++ test/StreamManagerTest.js
@@ -5,6 +5,10 @@ const { getSigners, getDeployContracts } = require("./fixtures");
 
 // TODO: rewrite tests
 describe.only("StreamManager:", async () => {
+	const amount = 1000
+    const rate = 1500
+    const terminationPeriod = 18 * 24 * 3600; // 18 days
+    const cliffPeriod = 24 * 3600; // 24 hrs 
 
 	it("Checking fixtures", async () => {
 		await loadFixture(getSigners)
@@ -107,6 +111,76 @@ describe.only("StreamManager:", async () => {
 			await expect(
 				streamManager.connect(admin).changePayerAddress(payer.address)
 			).to.be.revertedWith('InvalidAddress')
+		})
+	})
+
+	describe("deposit();", async () => {
+		// Tests for `deposit();`
+  		// Deposit USDT(mock)
+		it('Deposit succeed;', async () => {
+
+			const { admin, payer, payee1, payee2 } = await loadFixture(getSigners)
+	    	const { streamManager, mockUSDT } = await loadFixture(getDeployContracts)
+
+			await mockUSDT.mint(payer.address, amount)
+
+			await mockUSDT.connect(payer).approve(streamManager.address, amount)
+
+			await expect(
+			  	streamManager.connect(payer).deposit(
+			  	mockUSDT.address,
+			  	amount
+			))
+			.to.emit(streamManager, "TokensDeposited")
+			.withArgs(mockUSDT.address, amount)
+
+			expect(await mockUSDT.balanceOf(streamManager.address)).to.equal(amount)
+		})
+
+		// Expecting revert with `InvalidAddress`
+		it('Deposit: `_token` not set how address(0);', async () => {
+
+			const { admin, payer, payee1, payee2 } = await loadFixture(getSigners)
+	    	const { streamManager, mockUSDT } = await loadFixture(getDeployContracts)
+	    	const zero = ethers.constants.AddressZero
+
+		    // Setting `_token` = address(0)
+		    await expect(
+				streamManager.connect(payer).deposit(
+		    	zero,
+		      	amount
+		    ))
+		    .to.be.revertedWith('InvalidAddress');
+		})
+
+		// Expecting revert with `InvalidValue`
+		it('Deposit: `_amount` not set how 0;', async () => {
+
+			const { admin, payer, payee1, payee2 } = await loadFixture(getSigners)
+	    	const { streamManager, mockUSDT } = await loadFixture(getDeployContracts)
+
+			// Setting `_amount` = 0
+			await expect(
+			  	streamManager.connect(payer).deposit(
+			  	mockUSDT.address,
+			  	0
+			))
+			.to.be.revertedWith('InvalidValue');
+		})
+
+		// Expecting revert with `NotPayer`
+		it('Deposit: only payer can call this function;', async () => {
+
+			const { admin, payer, payee1, payee2 } = await loadFixture(getSigners)
+	    	const { streamManager, mockUSDT } = await loadFixture(getDeployContracts)
+
+			// Calling from other address
+			await expect(
+				streamManager.connect(payee1).deposit(
+				mockUSDT.address,
+				amount
+			))
+		    .to.be.revertedWith('NotPayer');
 		})
 	})
 });
